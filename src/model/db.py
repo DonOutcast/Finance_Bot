85cb2ef4ec03f2bfe9a68560333d5db44e7dcac2
DonOutcast <lymondgl@student.21-school.ru>
2023-02-22 08:56:56 +0300

    _fromat_kwargs in sql

--- src/model/db.py
+++ src/model/db.py
@@ -1,17 +1,18 @@
 import sqlite3
 
+
 class Database:
-    
+
     def __init__(self, path_to_db="database.db"):
         self.path_to_db = path_to_db
 
     @property
-    def connection(self):
+    def _connection(self):
         return sqlite3.connect(self.path_to_db)
 
-    def execute(self, sql: str, parameters: tuple = None, fethcone=False,
+    def execute(self, sql: str, parameters: tuple = None, fetchone=False,
                 fetchall=False, commit=False):
-        connection = self.connection
+        connection = self._connection
         connection.set_trace_callback(logger)
         cursor = connection.cursor()
         cursor.execute(sql, parameters)
@@ -25,5 +26,21 @@ class Database:
         connection.close()
         return data
 
+    @staticmethod
+    def _format_kwargs(sql: str, parameters: dict) -> tuple:
+        sql += " AND".join(
+            [
+                f"{item} = ?" for item in parameters
+            ]
+        )
+        return sql, tuple(parameters)
+
+
 def logger(statement):
     print(f"""{statement}""")
+
+
+
+
+
+
