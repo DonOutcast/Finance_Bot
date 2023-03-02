0d989e4cbf73c37b47702d4924ae29c7558857c3
DonOutcast <lymondgl@student.21-school.ru>
2023-03-02 23:34:25 +0300

    build teplates

---
+++ src/model/templates.py
@@ -0,0 +1,32 @@
+import re
+
+import jinja2
+
+from src.configurate import config
+
+
+def render_template(template_name: str, data: dict | None = None) -> str:
+    if data is None:
+        data = {}
+    template = _get_template_env().get_template(template_name)
+    rendered = template.render(**data).replace("\n", " ")
+    rendered = rendered.replace("<br>", "\n")
+    rendered = re.sub(" +", " ", rendered).replace(" .", ".").replace(" ,", ",")
+    rendered = "\n".join(line.strip() for line in rendered.split("\n"))
+    rendered = rendered.replace("{FOURPACES}", "    ")
+    return rendered
+
+
+def _get_template_env():
+    if not getattr(_get_template_env, "template_env", None):
+        template_loader = jinja2.FileSystemLoader(searchpath=config.TEMPLATES_DIR)
+        env = jinja2.Environment(
+            loader=template_loader,
+            trim_blocks=True,
+            lstrip_blocks=True,
+            autoescape=True,
+        )
+
+        _get_template_env.template_env = env
+
+    return _get_template_env.template_env
